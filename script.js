20e5c751bec57c33cd3ae58eb3eaae7fc94d0de2
alcantara98 <57881656+Elshtein99@users.noreply.github.com>
2020-11-12 23:32:34 +1300

    Finished

--- script.js
+++ script.js
@@ -2,5 +2,33 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+console.log(generateTarget());
 
+const compareGuesses = (human, comp, target) => {
+    let compDiff = Math.abs(target - comp);
+    let humanDiff = Math.abs(target - human);
+
+    if(compDiff < humanDiff){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+const updateScore = winner =>{
+    switch(winner){
+        case 'human':
+            humanScore++;
+            break;
+        case 'computer':
+            computerScore++;
+            break;
+    }
+}
+
+const advanceRound = () =>{
+    currentRoundNumber++;
+}
