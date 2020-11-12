da3f570a093309b92c7f2575d79a54bab5bc0420
alcantara98 <57881656+Elshtein99@users.noreply.github.com>
2020-11-12 23:36:56 +1300

    added input checker to alert player if input is out of range.

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ console.log(generateTarget());
 const compareGuesses = (human, comp, target) => {
     let compDiff = Math.abs(target - comp);
     let humanDiff = Math.abs(target - human);
-
+    if(human < 0 || human > 9){
+        alert("Invalid Number, choose from 0 to 9 inclusive.");
+    }
     if(compDiff < humanDiff){
         return false;
     }else{
